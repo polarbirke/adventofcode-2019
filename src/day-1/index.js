fa963ce83edb138618ed17dc641ad11db687ac4e
Søren Birkemeyer <sb@webfactory.de>
2019-12-02 22:47:08 +0100

    Solve Day 1

--- src/day-1/index.js
+++ src/day-1/index.js
@@ -4,21 +4,44 @@ import * as data from './input.json';
 const { input } = data;
 
 // calculate the required fuel for a given mass
-let calcRequiredFuelForMass = (mass) => {
-    return '';
-};
+let calcRequiredFuelForMass = (mass) => Math.floor(mass/3)-2;
 
 // calculate the required fuel for a given mass, but also take into account
 // that the fuel has mass and thus also requires fuel
 let calcRequiredFuelForMassAndFuel = (mass) => {
-    return '';
+    let fuel = calcRequiredFuelForMass(mass);
+    let totalfuel = 0;
+
+    for(fuel; fuel > 0; fuel = calcRequiredFuelForMass(fuel)) {
+        if (fuel > 0) {
+            totalfuel += fuel;
+        }
+    }
+
+    return totalfuel;
 };
 
 // calculate the total fuel requirement for part 1
-let calcTotalFuelForMasses = () => '';
+let calcTotalFuelForMasses = () => {
+    let totalFuel = 0;
+
+    input.forEach((mass) => {
+        totalFuel += calcRequiredFuelForMass(mass);
+    });
+
+    return totalFuel;
+};
 
 // calculate the total fuel requirement for part 2
-let calcTotalFuelForMassesAndFuel = () => '';
+let calcTotalFuelForMassesAndFuel = () => {
+    let totalFuel = 0;
+
+    input.forEach((mass) => {
+        totalFuel += calcRequiredFuelForMassAndFuel(mass);
+    });
+
+    return totalFuel;
+};
 
 export {
     calcRequiredFuelForMass,
